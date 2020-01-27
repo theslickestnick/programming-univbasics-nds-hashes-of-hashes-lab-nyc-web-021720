c8529dbe719bd62b869d31705130b2ec7fcd8a60
Nicholas Echevarria <nick.echev@gmail.com>
2020-01-27 20:04:00 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -43,26 +43,26 @@ def naming_system
   # HoH all the way to Species!
   animal_kingdom = {
     PORTION_1 = {
-      label: "Kingdom",
-      sub_category: {
-        label: "Phylum",
-        sub_category: {
-          label: "Class"
+      :label => "Kingdom",
+      :sub_category => {
+        :label => "Phylum",
+        :sub_category => {
+          :label => "Class"
         }
       }
     },
     PORTION_2 = {
-      label: "Order"
+      :label => "Order"
     },
     PORTION_3 = {
-      label: "Family",
-      sub_category: {
-        label: "Genus",
+      :label => "Family",
+      :sub_category => {
+        :label => "Genus",
       }
     },
     PORTION_4 = {
-      label: "Species",
-      sub_category: nil
+      :label => "Species",
+      :sub_category => nil
     }
   }
 end
